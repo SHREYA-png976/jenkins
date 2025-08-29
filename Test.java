0686d2b8e8782bfbec47be820f1c67d4613cc776
SHREYA-png976 <shreyarajakumar2003@gmail.com>
2025-08-29 10:30:30 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test{
             public static void main(string[] args){
-              system.out.println("Hello World");
+              System.out.println("Hello World");
             }
 }
